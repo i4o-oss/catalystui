6591404b20712b31e8f982da20b091e50f4f9612
Ilango <rajagopalilango@gmail.com>
2023-01-04 15:35:59 +0530

    👌 IMPROVE: Update default styles

--- docs/theme.config.js
+++ docs/theme.config.js
@@ -1,5 +1,18 @@
+import Image from 'next/image'
+
 const theme = {
-	logo: <span>Catalyst UI</span>,
+	logo: (
+		<span className='flex items-center space-x-2'>
+			<Image
+				src='/assets/logo.png'
+				alt='CatalystUI Logo'
+				width={40}
+				height={40}
+				priority
+			/>
+			<span className='text-base font-semibold'>Catalyst UI</span>
+		</span>
+	),
 	project: {
 		link: 'https://github.com/i4o-oss/catalystui',
 	},
