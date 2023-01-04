589d2eacbafc1d9ecb60cb2587a7e85c43c56701
Ilango <rajagopalilango@gmail.com>
2023-01-04 15:25:57 +0530

    👌 IMPROVE: Update docs home page

---
+++ docs/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				brand: {
+					DEFAULT: '#2CB67D',
+					50: '#E6F9F1',
+					100: '#D2F4E6',
+					200: '#A5E9CD',
+					300: '#77DEB3',
+					400: '#46D298',
+					500: '#2CB67D',
+					600: '#239062',
+					700: '#1B6F4C',
+					800: '#124A33',
+					900: '#092519',
+				},
+			},
+			fontFamily: {
+				sans: ['Inter', 'sans-serif'],
+				serif: ['Merriweather', 'serif'],
+			},
+		},
+	},
+	plugins: [],
+}
