dcccd029ffb32a953debd6f684a2b73d309d7109
Ilango <rajagopalilango@gmail.com>
2023-01-05 13:25:47 +0530

    👌 IMPROVE: Update avatar and dropdown components

--- packages/ui/src/avatar.tsx
+++ packages/ui/src/avatar.tsx
@@ -2,39 +2,28 @@ import type { FC, ReactNode } from 'react'
 import * as AvatarPrimitive from '@radix-ui/react-avatar'
 import cx from 'classnames'
 
-export enum Variant {
-	Circle,
-	Rounded,
-}
-
 interface AvatarProps {
 	alt?: string
 	fallback?: ReactNode | string
 	src?: string
-	variant?: Variant
+	variant?: 'circle' | 'rounded'
 }
 
 const Avatar: FC<AvatarProps> = ({ alt, fallback, src, variant }) => {
 	return (
-		<AvatarPrimitive.Root className='relative inline-flex h-10 w-10'>
+		<AvatarPrimitive.Root className='relative inline-flex h-10 w-10 overflow-hidden'>
 			<AvatarPrimitive.Image
 				alt={alt}
 				className={cx(
-					'h-full w-full object-cover',
-					{
-						[Variant.Circle]: 'rounded-full',
-						[Variant.Rounded]: 'rounded',
-					}[variant as number]
+					'h-full w-full object-cover shadow-md',
+					`${variant === 'circle' ? 'rounded-full' : 'rounded'}`
 				)}
 				src={src}
 			/>
 			<AvatarPrimitive.Fallback
 				className={cx(
-					'flex h-full w-full items-center justify-center bg-white dark:bg-gray-800',
-					{
-						[Variant.Circle]: 'rounded-full',
-						[Variant.Rounded]: 'rounded',
-					}[variant as number]
+					'flex h-full w-full items-center justify-center bg-gray-200 dark:bg-gray-800 shadow-md',
+					`${variant === 'circle' ? 'rounded-full' : 'rounded'}`
 				)}
 				delayMs={500}
 			>
--- packages/ui/src/dropdown.tsx
+++ packages/ui/src/dropdown.tsx
@@ -2,19 +2,21 @@ import type { FC, ReactNode } from 'react'
 import { Link } from '@remix-run/react'
 import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu'
 import cx from 'classnames'
+import { ChevronRightIcon } from '@radix-ui/react-icons'
 
-interface DropdownMenuItemType {
+interface DropdownMenuItem {
 	icon?: ReactNode
-	label: string | ReactNode
+	label?: string | ReactNode
 	link?: string
 	onSelect?: (e: Event) => void
 	shortcut?: string
+	type: 'item' | 'separator' | 'submenu'
+	submenu?: DropdownMenuItem[]
 }
 
 interface DropdownMenuProps {
 	align?: 'end' | 'start' | 'center' | undefined
-	items: DropdownMenuItemType[]
-	secondaryItems?: DropdownMenuItemType[]
+	items: DropdownMenuItem[]
 	sideOffset?: number
 	trigger: ReactNode
 }
@@ -22,7 +24,6 @@ interface DropdownMenuProps {
 const DropdownMenu: FC<DropdownMenuProps> = ({
 	align = 'end',
 	items,
-	secondaryItems,
 	sideOffset = 5,
 	trigger,
 }) => {
@@ -42,104 +43,178 @@ const DropdownMenu: FC<DropdownMenuProps> = ({
 					sideOffset={sideOffset}
 				>
 					{items.map(
-						({ label, link, icon, onSelect, shortcut }, i) => {
-							return link ? (
-								<Link key={i} to={link}>
+						(
+							{
+								label,
+								link,
+								icon,
+								onSelect,
+								shortcut,
+								type,
+								submenu,
+							},
+							i
+						) => {
+							if (type === 'separator') {
+								return (
+									<DropdownMenuPrimitive.Separator
+										key={i}
+										className='my-1 h-px bg-gray-200 dark:bg-gray-700'
+									/>
+								)
+							} else if (type === 'submenu') {
+								return (
+									<DropdownMenuPrimitive.Sub key={i}>
+										<DropdownMenuPrimitive.SubTrigger
+											className={cx(
+												'flex cursor-pointer select-none items-center space-x-2 rounded-md px-2 py-2 outline-none',
+												'text-gray-600 focus:bg-gray-50 dark:text-gray-400 dark:focus:bg-gray-900'
+											)}
+										>
+											<div className='basis-4'>
+												{icon}
+											</div>
+											<div className='text-sm font-normal flex-1'>
+												{label}
+											</div>
+											<ChevronRightIcon className='w-4 h-4' />
+										</DropdownMenuPrimitive.SubTrigger>
+										<DropdownMenuPrimitive.SubContent
+											className={cx(
+												' radix-side-top:animate-slide-up radix-side-bottom:animate-slide-down',
+												'w-48 rounded-lg px-1.5 py-1 shadow-lg md:w-56',
+												'bg-white dark:bg-gray-800'
+											)}
+											sideOffset={sideOffset}
+											alignOffset={-5}
+										>
+											{submenu?.map(
+												(
+													{
+														label,
+														link,
+														icon,
+														onSelect,
+														shortcut,
+														type,
+													},
+													j
+												) => {
+													if (type === 'separator') {
+														return (
+															<DropdownMenuPrimitive.Separator
+																key={`${i}-${j}`}
+																className='my-1 h-px bg-gray-200 dark:bg-gray-700'
+															/>
+														)
+													} else {
+														return link ? (
+															<Link
+																key={`${i}-${j}`}
+																to={link}
+															>
+																<DropdownMenuPrimitive.Item
+																	key={i}
+																	className={cx(
+																		'flex cursor-pointer select-none items-center space-x-2 rounded-md px-2 py-2 outline-none',
+																		'text-gray-600 focus:bg-gray-50 dark:text-gray-400 dark:focus:bg-gray-900'
+																	)}
+																	onSelect={
+																		onSelect
+																	}
+																>
+																	<div className='basis-4'>
+																		{icon}
+																	</div>
+																	<div className='text-sm font-normal flex-1'>
+																		{label}
+																	</div>
+																	{shortcut && (
+																		<div className='text-xs font-normal text-gray-400 dark:text-gray-600'>
+																			{
+																				shortcut
+																			}
+																		</div>
+																	)}
+																</DropdownMenuPrimitive.Item>
+															</Link>
+														) : (
+															<DropdownMenuPrimitive.Item
+																key={`${i}-${j}`}
+																className={cx(
+																	'flex cursor-pointer select-none items-center space-x-2 rounded-md px-2 py-2 outline-none',
+																	'text-gray-600 focus:bg-gray-50 dark:text-gray-400 dark:focus:bg-gray-900'
+																)}
+																onSelect={
+																	onSelect
+																}
+															>
+																<div className='basis-4'>
+																	{icon}
+																</div>
+																<div className='text-sm font-normal flex-1'>
+																	{label}
+																</div>
+																{shortcut && (
+																	<div className='text-xs font-normal text-gray-400 dark:text-gray-600'>
+																		{
+																			shortcut
+																		}
+																	</div>
+																)}
+															</DropdownMenuPrimitive.Item>
+														)
+													}
+												}
+											)}
+										</DropdownMenuPrimitive.SubContent>
+									</DropdownMenuPrimitive.Sub>
+								)
+							} else {
+								return link ? (
+									<Link key={i} to={link}>
+										<DropdownMenuPrimitive.Item
+											key={i}
+											className={cx(
+												'flex cursor-pointer select-none items-center space-x-2 rounded-md px-2 py-2 outline-none',
+												'text-gray-600 focus:bg-gray-50 dark:text-gray-400 dark:focus:bg-gray-900'
+											)}
+											onSelect={onSelect}
+										>
+											<div className='basis-4'>
+												{icon}
+											</div>
+											<div className='text-sm font-normal flex-1'>
+												{label}
+											</div>
+											{shortcut && (
+												<div className='text-xs font-normal text-gray-400 dark:text-gray-600'>
+													{shortcut}
+												</div>
+											)}
+										</DropdownMenuPrimitive.Item>
+									</Link>
+								) : (
 									<DropdownMenuPrimitive.Item
 										key={i}
 										className={cx(
-											'flex cursor-pointer select-none items-center space-x-2 rounded-md px-2 py-2 text-sm outline-none',
+											'flex cursor-pointer select-none items-center space-x-2 rounded-md px-2 py-2 outline-none',
 											'text-gray-600 focus:bg-gray-50 dark:text-gray-400 dark:focus:bg-gray-900'
 										)}
 										onSelect={onSelect}
 									>
-										{icon && (
-											<div className='flex-shrink-0'>
-												{icon}
-											</div>
-										)}
-										<div className='flex-1'>{label}</div>
+										<div className='basis-4'>{icon}</div>
+										<div className='text-sm font-normal flex-1'>
+											{label}
+										</div>
 										{shortcut && (
-											<div className='text-sm text-gray-600 dark:text-gray-400'>
+											<div className='text-xs font-normal text-gray-400 dark:text-gray-600'>
 												{shortcut}
 											</div>
 										)}
 									</DropdownMenuPrimitive.Item>
-								</Link>
-							) : (
-								<DropdownMenuPrimitive.Item
-									key={i}
-									className={cx(
-										'flex cursor-pointer select-none items-center space-x-2 rounded-md px-2 py-2 text-sm outline-none',
-										'text-gray-600 focus:bg-gray-50 dark:text-gray-400 dark:focus:bg-gray-900'
-									)}
-									onSelect={onSelect}
-								>
-									{icon && (
-										<div className='flex-shrink-0'>
-											{icon}
-										</div>
-									)}
-									<div className='flex-1'>{label}</div>
-									{shortcut && (
-										<div className='text-sm text-gray-600 dark:text-gray-400'>
-											{shortcut}
-										</div>
-									)}
-								</DropdownMenuPrimitive.Item>
-							)
-						}
-					)}
-					{secondaryItems && (
-						<DropdownMenuPrimitive.Separator className='my-1 h-px bg-gray-200 dark:bg-gray-700' />
-					)}
-					{secondaryItems?.map(
-						({ label, link, icon, onSelect, shortcut }, i) => {
-							return link ? (
-								<Link key={i} to={link}>
-									<DropdownMenuPrimitive.Item
-										key={i}
-										className={cx(
-											'flex cursor-pointer select-none items-center space-x-2 rounded-md px-2 py-2 text-sm outline-none',
-											'text-gray-600 focus:bg-gray-50 dark:text-gray-400 dark:focus:bg-gray-900'
-										)}
-										onSelect={onSelect}
-									>
-										{icon && (
-											<div className='flex-shrink-0'>
-												{icon}
-											</div>
-										)}
-										<div className='flex-1'>{label}</div>
-										{shortcut && (
-											<div className='text-sm text-gray-600 dark:text-gray-400'>
-												{shortcut}
-											</div>
-										)}
-									</DropdownMenuPrimitive.Item>
-								</Link>
-							) : (
-								<DropdownMenuPrimitive.Item
-									key={i}
-									className={cx(
-										'flex cursor-pointer select-none items-center space-x-2 rounded-md px-2 py-2 text-sm outline-none',
-										'text-gray-600 focus:bg-gray-50 dark:text-gray-400 dark:focus:bg-gray-900'
-									)}
-									onSelect={onSelect}
-								>
-									{icon && (
-										<div className='flex-shrink-0'>
-											{icon}
-										</div>
-									)}
-									<div className='flex-1'>{label}</div>
-									{shortcut && (
-										<div className='text-sm text-gray-600 dark:text-gray-400'>
-											{shortcut}
-										</div>
-									)}
-								</DropdownMenuPrimitive.Item>
-							)
+								)
+							}
 						}
 					)}
 				</DropdownMenuPrimitive.Content>
