ee4d3530980b596d45f76d588372b39b490dea33
Ilango <rajagopalilango@gmail.com>
2023-01-05 13:26:04 +0530

    👌 IMPROVE: Update demo card component in docs

--- docs/components/demo-card.tsx
+++ docs/components/demo-card.tsx
@@ -1,10 +1,13 @@
 type DemoCardProps = {
 	children: React.ReactNode
+	className?: string
 }
 
 export default function DemoCard(props: DemoCardProps) {
 	return (
-		<div className='w-full border border-gray-200 dark:border-gray-800 px-8 py-16 flex items-center justify-center rounded-lg my-4'>
+		<div
+			className={`${props.className} w-full border border-gray-200 dark:border-gray-800 px-8 py-16 flex items-center justify-center rounded-lg my-4 bg-gradient-to-l from-orange-500 to-yellow-300`}
+		>
 			{props.children}
 		</div>
 	)
