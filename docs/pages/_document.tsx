e0fe7d3ade5ebc394f9d001e34f0a49bfe71a975
Ilango <rajagopalilango@gmail.com>
2023-01-08 08:00:00 +0530

    👌 IMPROVE: Update docs home page

--- docs/pages/_document.tsx
+++ docs/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
 	return (
 		<Html lang='en'>
-			<Head />
+			<Head>
+				<link rel='preconnect' href='https://fonts.googleapis.com' />
+				<link rel='preconnect' href='https://fonts.gstatic.com' />
+				<link
+					rel='stylesheet'
+					href='https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;600;700&amp;family=Merriweather:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&amp;family=Orbitron:wght@700&amp;display=swap'
+				/>
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
--- docs/theme.config.js
+++ docs/theme.config.js
@@ -15,6 +15,43 @@ const theme = {
 	),
 	project: {
 		link: 'https://github.com/i4o-oss/catalystui',
+		newWindow: true,
+	},
+	head: ({ title }) => {
+		return (
+			<>
+				<link
+					rel='apple-touch-icon'
+					sizes='180x180'
+					href='/assets/apple-touch-icon.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='32x32'
+					href='/assets/favicon-32x32.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='16x16'
+					href='/assets/favicon-16x16.png'
+				/>
+				<link rel='manifest' href='/assets/site.webmanifest' />
+				<link
+					rel='mask-icon'
+					href='/assets/safari-pinned-tab.svg'
+					color='#5bbad5'
+				/>
+				<meta name='msapplication-TileColor' content='#da532c' />
+				<meta name='theme-color' content='#ffffff' />
+				<title>
+					{title
+						? `${title} - Catalyst UI`
+						: 'Styled Radix UI components and utilities — Catalyst UI'}
+				</title>
+			</>
+		)
 	},
 }
 export default theme
