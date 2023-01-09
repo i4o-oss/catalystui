bbdddd5959234e1afc620c9d562ee4f39d300e39
Ilango <rajagopalilango@gmail.com>
2023-01-09 12:39:23 +0530

    👌 IMPROVE: Update components used by docs

---
+++ docs/components/component-link.tsx
@@ -0,0 +1,20 @@
+import type { ReactNode } from 'react'
+import Link from 'next/link'
+
+type ComponentLinkProps = {
+	children: ReactNode
+	className?: string
+	href: string
+}
+
+export default function ComponentLink(props: ComponentLinkProps) {
+	return (
+		<Link href={props.href}>
+			<div
+				className={`${props.className} w-full border border-gray-200 dark:border-gray-800 px-8 py-16 flex items-center justify-center rounded-lg`}
+			>
+				{props.children}
+			</div>
+		</Link>
+	)
+}
--- docs/components/demo-card.tsx
+++ docs/components/demo-card.tsx
@@ -1,5 +1,7 @@
+import type { ReactNode } from 'react'
+
 type DemoCardProps = {
-	children: React.ReactNode
+	children: ReactNode
 	className?: string
 }
 
