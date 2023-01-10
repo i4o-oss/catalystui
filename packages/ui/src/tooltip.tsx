4da1c7b52d7157358b7eae5b84c3b880ad2d52b3
Ilango <rajagopalilango@gmail.com>
2023-01-10 10:03:25 +0530

    👌 IMPROVE: Add docs for tooltip and add more props to tooltip component

--- packages/ui/src/tooltip.tsx
+++ packages/ui/src/tooltip.tsx
@@ -3,18 +3,26 @@ import * as TooltipPrimitive from '@radix-ui/react-tooltip'
 import cx from 'classnames'
 
 interface Props {
+	align?: 'start' | 'center' | 'end'
 	content: string | ReactNode
 	children: ReactNode
+	side?: 'top' | 'right' | 'bottom' | 'left'
 }
 
-const Tooltip: FC<Props> = ({ content, children }) => {
+const Tooltip: FC<Props> = ({
+	align = 'center',
+	content,
+	children,
+	side = 'top',
+}) => {
 	return (
 		<TooltipPrimitive.Provider>
 			<TooltipPrimitive.Root>
 				<TooltipPrimitive.Trigger>{children}</TooltipPrimitive.Trigger>
 				<TooltipPrimitive.Portal>
 					<TooltipPrimitive.Content
-						side='top'
+						align={align}
+						side={side}
 						sideOffset={4}
 						className={cx(
 							'radix-side-top:animate-slide-down-fade',
@@ -22,11 +30,12 @@ const Tooltip: FC<Props> = ({ content, children }) => {
 							'radix-side-bottom:animate-slide-up-fade',
 							'radix-side-left:animate-slide-right-fade',
 							'inline-flex items-center rounded-md px-4 py-2.5',
-							'bg-white dark:bg-gray-800'
+							'bg-white dark:bg-gray-800',
+							'max-w-xs'
 						)}
 					>
 						<TooltipPrimitive.Arrow className='fill-current text-white dark:text-gray-800' />
-						<span className='block text-xs leading-none text-gray-700 dark:text-gray-100'>
+						<span className='block text-xs leading-none text-gray-700 dark:text-gray-100 leading-normal'>
 							{content}
 						</span>
 					</TooltipPrimitive.Content>
