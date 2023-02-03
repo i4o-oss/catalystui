91f1b0137ef0282861e8f3efd2f701bb5203202f
Ilango <rajagopalilango@gmail.com>
2023-02-04 00:44:49 +0530

    🐛 FIX: Revert adding footer

--- docs/theme.config.js
+++ docs/theme.config.js
@@ -54,62 +54,62 @@ const theme = {
 		)
 	},
 	gitTimestamp: null,
-	footer: {
-		component: (
-			<div className='w-screen h-20 sticky top-0 z-50 flex flex-wrap items-center justify-center px-4 sm:px-6 lg:px-8 py-4 border-t border-gray-200'>
-				<div className='w-[88rem] flex items-center justify-between'>
-					<p className='flex items-center text-gray-400 gap-2'>
-						<svg
-							xmlns='http://www.w3.org/2000/svg'
-							width='24'
-							height='24'
-							viewBox='0 0 24 24'
-							fill='none'
-							stroke='currentColor'
-							stroke-width='2'
-							stroke-linecap='round'
-							stroke-linejoin='round'
-						>
-							<path d='m7 11 2-2-2-2'></path>
-							<path d='M11 13h4'></path>
-							<rect
-								x='3'
-								y='3'
-								width='18'
-								height='18'
-								rx='2'
-								ry='2'
-							></rect>
-						</svg>
-						<p className=''>
-							Built by{' '}
-							<a
-								className='underline'
-								href='https://i4o.dev'
-								target='_blank'
-								rel='noreferrer noopener'
-							>
-								i4o
-							</a>
-							.
-						</p>
-					</p>
-					<div className='flex items-center justify-end'>
-						<p>
-							The source code is available on{' '}
-							<a
-								aria-label='Github Repo'
-								href='https://github.com/i4o-oss/catalystui'
-								target='_blank'
-								rel='noreferrer noopener'
-							>
-								Github
-							</a>
-						</p>
-					</div>
-				</div>
-			</div>
-		),
-	},
+	// footer: {
+	// 	component: (
+	// 		<div className='w-screen h-20 sticky top-0 z-50 flex flex-wrap items-center justify-center px-4 sm:px-6 lg:px-8 py-4 border-t border-gray-200'>
+	// 			<div className='w-[88rem] flex items-center justify-between'>
+	// 				<p className='flex items-center text-gray-400 gap-2'>
+	// 					<svg
+	// 						xmlns='http://www.w3.org/2000/svg'
+	// 						width='24'
+	// 						height='24'
+	// 						viewBox='0 0 24 24'
+	// 						fill='none'
+	// 						stroke='currentColor'
+	// 						stroke-width='2'
+	// 						stroke-linecap='round'
+	// 						stroke-linejoin='round'
+	// 					>
+	// 						<path d='m7 11 2-2-2-2'></path>
+	// 						<path d='M11 13h4'></path>
+	// 						<rect
+	// 							x='3'
+	// 							y='3'
+	// 							width='18'
+	// 							height='18'
+	// 							rx='2'
+	// 							ry='2'
+	// 						></rect>
+	// 					</svg>
+	// 					<p className=''>
+	// 						Built by{' '}
+	// 						<a
+	// 							className='underline'
+	// 							href='https://i4o.dev'
+	// 							target='_blank'
+	// 							rel='noreferrer noopener'
+	// 						>
+	// 							i4o
+	// 						</a>
+	// 						.
+	// 					</p>
+	// 				</p>
+	// 				<div className='flex items-center justify-end'>
+	// 					<p>
+	// 						The source code is available on{' '}
+	// 						<a
+	// 							aria-label='Github Repo'
+	// 							href='https://github.com/i4o-oss/catalystui'
+	// 							target='_blank'
+	// 							rel='noreferrer noopener'
+	// 						>
+	// 							Github
+	// 						</a>
+	// 					</p>
+	// 				</div>
+	// 			</div>
+	// 		</div>
+	// 	),
+	// },
 }
 export default theme
