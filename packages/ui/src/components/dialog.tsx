7aa4014900c3c658c4543d074c25b11612b5cb58
Ilango <rajagopalilango@gmail.com>
2023-03-08 22:39:28 +0530

    👌 IMPROVE: Update dialog styles

--- packages/ui/src/components/dialog.tsx
+++ packages/ui/src/components/dialog.tsx
@@ -40,7 +40,7 @@ const Dialog: FC<Props> = ({
 					forceMount
 					className={cx(
 						'fixed z-50',
-						'w-[95vw] max-w-md rounded-lg p-4 md:w-full',
+						'min-w-[96vw] rounded-lg p-4 md:w-full',
 						'top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]',
 						'bg-white dark:bg-gray-800',
 						'focus:outline-none focus-visible:ring focus-visible:ring-brand-500 focus-visible:ring-opacity-75'
