7dc7b66ee848af61c40236b51e2233bcfeec6c73
Ilango <rajagopalilango@gmail.com>
2023-03-08 13:22:13 +0530

    👌 IMPROVE: Update alert and dialog components

--- packages/ui/src/components/alert.tsx
+++ packages/ui/src/components/alert.tsx
@@ -1,7 +1,5 @@
-import type { FC, ReactNode } from 'react'
-import { useState, Fragment } from 'react'
+import type { Dispatch, FC, ReactNode, SetStateAction } from 'react'
 import * as AlertPrimitive from '@radix-ui/react-alert-dialog'
-import { Transition } from '@headlessui/react'
 import cx from 'classnames'
 
 type AlertProps = {
@@ -11,6 +9,7 @@ type AlertProps = {
 	action: ReactNode
 	trigger?: ReactNode
 	isOpen?: boolean
+	onOpenChange?: Dispatch<SetStateAction<boolean>>
 }
 
 const Alert: FC<AlertProps> = ({
@@ -20,72 +19,45 @@ const Alert: FC<AlertProps> = ({
 	action,
 	trigger = null,
 	isOpen,
+	onOpenChange,
 }) => {
-	let [isAlertOpen, setIsAlertOpen] = useState(isOpen || false)
-
 	return (
-		<AlertPrimitive.Root open={isAlertOpen} onOpenChange={setIsAlertOpen}>
+		<AlertPrimitive.Root open={isOpen} onOpenChange={onOpenChange}>
 			{trigger && (
 				<AlertPrimitive.Trigger asChild>
 					{trigger}
 				</AlertPrimitive.Trigger>
 			)}
 			<AlertPrimitive.Portal>
-				<Transition.Root show={isAlertOpen}>
-					<Transition.Child
-						as={Fragment}
-						enter='ease-out duration-300'
-						enterFrom='opacity-0'
-						enterTo='opacity-100'
-						leave='ease-in duration-200'
-						leaveFrom='opacity-100'
-						leaveTo='opacity-0'
-					>
-						<AlertPrimitive.Overlay
-							forceMount
-							className='fixed inset-0 z-20 bg-black/50'
-						/>
-					</Transition.Child>
-					<Transition.Child
-						as={Fragment}
-						enter='ease-out duration-300'
-						enterFrom='opacity-0 scale-95'
-						enterTo='opacity-100 scale-100'
-						leave='ease-in duration-200'
-						leaveFrom='opacity-100 scale-100'
-						leaveTo='opacity-0 scale-95'
-					>
-						<AlertPrimitive.Content
-							forceMount
-							className={cx(
-								'fixed z-50',
-								'w-[95vw] max-w-md rounded-lg p-4 md:w-full',
-								'top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]',
-								'bg-white dark:bg-gray-800',
-								'focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75'
-							)}
-						>
-							{title && (
-								<AlertPrimitive.Title className='text-sm font-medium text-gray-900 dark:text-gray-100'>
-									{title}
-								</AlertPrimitive.Title>
-							)}
-							{description && (
-								<AlertPrimitive.Description className='mt-2 text-sm font-normal text-gray-700 dark:text-gray-400'>
-									{description}
-								</AlertPrimitive.Description>
-							)}
-							<div className='mt-4 flex justify-end space-x-2'>
-								<AlertPrimitive.Cancel>
-									{cancel}
-								</AlertPrimitive.Cancel>
-								<AlertPrimitive.Action>
-									{action}
-								</AlertPrimitive.Action>
-							</div>
-						</AlertPrimitive.Content>
-					</Transition.Child>
-				</Transition.Root>
+				<AlertPrimitive.Overlay
+					forceMount
+					className='fixed inset-0 z-20 bg-black/50'
+				/>
+				<AlertPrimitive.Content
+					forceMount
+					className={cx(
+						'fixed z-50',
+						'w-[95vw] max-w-md rounded-lg p-4 md:w-full',
+						'top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]',
+						'bg-white dark:bg-gray-800',
+						'focus:outline-none focus-visible:ring focus-visible:ring-brand-500 focus-visible:ring-opacity-75'
+					)}
+				>
+					{title && (
+						<AlertPrimitive.Title className='text-sm font-medium text-gray-900 dark:text-gray-100'>
+							{title}
+						</AlertPrimitive.Title>
+					)}
+					{description && (
+						<AlertPrimitive.Description className='mt-2 text-sm font-normal text-gray-700 dark:text-gray-400'>
+							{description}
+						</AlertPrimitive.Description>
+					)}
+					<div className='mt-4 flex justify-end space-x-2'>
+						<AlertPrimitive.Cancel>{cancel}</AlertPrimitive.Cancel>
+						<AlertPrimitive.Action>{action}</AlertPrimitive.Action>
+					</div>
+				</AlertPrimitive.Content>
 			</AlertPrimitive.Portal>
 		</AlertPrimitive.Root>
 	)
