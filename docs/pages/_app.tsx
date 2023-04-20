db08de6a25a28ddcfb23779e343cbd6e51eba875
Ilango <rajagopalilango@gmail.com>
2023-04-20 21:16:03 +0530

    🐛 FIX: Fix css file ordering

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,5 +1,5 @@
-import '../styles/globals.css'
 import '@i4o/catalystui/main.css'
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
