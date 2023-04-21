2a31d3ee53a4068f78d30bc809a59bee07ae11c7
Ilango <rajagopalilango@gmail.com>
2023-04-22 03:56:25 +0530

    👌 IMPROVE: Add new tag for collapsible item in sidebar

--- docs/theme.config.js
+++ docs/theme.config.js
@@ -58,6 +58,7 @@ const theme = {
 		titleComponent({ title }) {
 			if (
 				title === 'Aspect Ratio' ||
+				title === 'Collapsible' ||
 				title === 'Hover Card' ||
 				title === 'Progress'
 			) {
