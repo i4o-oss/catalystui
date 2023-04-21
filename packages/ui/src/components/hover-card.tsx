eb1ab3f57761018f9f2c724b0e94a6437a1c2c3c
Ilango <rajagopalilango@gmail.com>
2023-04-22 02:41:27 +0530

    📦 NEW: Add hover card component

---
+++ packages/ui/src/components/hover-card.tsx
@@ -0,0 +1,52 @@
+import type { FC, ReactNode } from 'react'
+import * as HoverCardPrimitive from '@radix-ui/react-hover-card'
+import cx from 'classnames'
+import { Button } from './buttons'
+
+interface HoverCardProps {
+	children: ReactNode
+	defaultOpen?: boolean
+	open?: boolean
+	onOpenChange?: (open: boolean) => void
+	trigger: ReactNode | string
+}
+
+const HoverCard: FC<HoverCardProps> = ({
+	children,
+	defaultOpen,
+	open,
+	onOpenChange,
+	trigger,
+}) => {
+	return (
+		<HoverCardPrimitive.Root
+			defaultOpen={defaultOpen}
+			open={open}
+			onOpenChange={onOpenChange}
+		>
+			<HoverCardPrimitive.Trigger asChild={typeof trigger !== 'string'}>
+				{typeof trigger !== 'string' ? (
+					trigger
+				) : (
+					<Button>{trigger}</Button>
+				)}
+			</HoverCardPrimitive.Trigger>
+			<HoverCardPrimitive.Content
+				align='center'
+				sideOffset={4}
+				className={cx(
+					'radix-side-top:cui-animate-slide-up radix-side-bottom:cui-animate-slide-down',
+					'cui-max-w-md cui-rounded-lg cui-p-4 md:cui-w-full',
+					'cui-bg-ui',
+					'cui-z-[100] cui-shadow-lg',
+					'focus:cui-outline-none focus-visible:cui-ring focus-visible:cui-ring-brand focus-visible:cui-ring-opacity-75'
+				)}
+			>
+				<HoverCardPrimitive.Arrow className='cui-fill-current cui-text-ui' />
+				{children}
+			</HoverCardPrimitive.Content>
+		</HoverCardPrimitive.Root>
+	)
+}
+
+export default HoverCard
