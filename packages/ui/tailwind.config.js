8e7c156078d1060c734fe7e355175eb5f0ffc50a
Ilango <rajagopalilango@gmail.com>
2023-04-21 15:07:00 +0530

    👌 IMPROVE: Update tailwind config and default styles

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -7,6 +7,17 @@ module.exports = {
 	prefix: 'cui-',
 	theme: {
 		extend: {
+			animation: {
+				'toast-hide': 'toast-hide 100ms ease-in forwards',
+				'toast-slide-in-right':
+					'toast-slide-in-right 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+				'toast-slide-in-bottom':
+					'toast-slide-in-bottom 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+				'toast-swipe-out-x':
+					'toast-swipe-out-x 100ms ease-out forwards',
+				'toast-swipe-out-y':
+					'toast-swipe-out-y 100ms ease-out forwards',
+			},
 			borderRadius: {
 				DEFAULT: 'var(--ui-border-radius)',
 			},
@@ -48,6 +59,37 @@ module.exports = {
 				mono: 'var(--font-mono)',
 				sans: 'var(--font-sans)',
 				serif: 'var(--font-serif)',
+			},
+			keyframes: {
+				// toast
+				'toast-hide': {
+					'0%': { opacity: 1 },
+					'100%': { opacity: 0 },
+				},
+				'toast-slide-in-right': {
+					'0%': { transform: `translateX(calc(100% + 1rem))` },
+					'100%': { transform: 'translateX(0)' },
+				},
+				'toast-slide-in-bottom': {
+					'0%': { transform: `translateY(calc(100% + 1rem))` },
+					'100%': { transform: 'translateY(0)' },
+				},
+				'toast-swipe-out-x': {
+					'0%': {
+						transform: 'translateX(var(--radix-toast-swipe-end-x))',
+					},
+					'100%': {
+						transform: `translateX(calc(100% + 1rem))`,
+					},
+				},
+				'toast-swipe-out-y': {
+					'0%': {
+						transform: 'translateY(var(--radix-toast-swipe-end-y))',
+					},
+					'100%': {
+						transform: `translateY(calc(100% + 1rem))`,
+					},
+				},
 			},
 			padding: {
 				DEFAULT: 'var(--ui-p)',
