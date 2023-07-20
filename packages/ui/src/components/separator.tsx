6298dd845129f1ee63206448084b82eb061d7554
Ilango <rajagopalilango@gmail.com>
2023-07-20 11:41:21 +0530

    📦 NEW: Add separator component

---
+++ packages/ui/src/components/separator.tsx
@@ -0,0 +1,24 @@
+import type { FC } from 'react'
+import * as SeparatorPrimitive from '@radix-ui/react-separator'
+import cx from 'classnames'
+
+interface SeparatorProps {
+	decorative?: boolean
+	orientation: 'horizontal' | 'vertical'
+}
+
+const Separator: FC<SeparatorProps> = ({ decorative = true, orientation }) => {
+	return (
+		<SeparatorPrimitive.Root
+			className={cx(
+				'cui-bg-subtle',
+                'data-[orientation=horizontal]:cui-h-px data-[orientation=horizontal]:cui-w-full data-[orientation=horizontal]:cui-my-4',
+                'data-[orientation=vertical]:cui-h-full  data-[orientation=vertical]:cui-w-px data-[orientation=vertical]:cui-mx-4'
+			)}
+			decorative={decorative}
+			orientation={orientation}
+		/>
+	)
+}
+
+export default Separator
