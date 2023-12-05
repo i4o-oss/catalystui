607ead2153308d78936607e02c56f11d60648fac
Kinfe123 <kinfishtech@gmail.com>
2023-12-06 00:11:03 +0300

    fix: copy verification

--- packages/ui/src/components/copy-to-clipboard.tsx
+++ packages/ui/src/components/copy-to-clipboard.tsx
@@ -1,6 +1,7 @@
-import type { FC } from 'react'
+'use client'
+import { useState, type FC } from 'react'
 import copy from 'copy-to-clipboard'
-import { CopyIcon } from '@radix-ui/react-icons'
+import { CopyIcon, CheckIcon } from '@radix-ui/react-icons'
 import { IconButton } from './buttons'
 import Tooltip from './tooltip'
 
@@ -9,16 +10,24 @@ interface Props {
 }
 
 const CopyToClipboard: FC<Props> = ({ text }) => {
+	const [clicked, setClicked] = useState(false)
+	const handleClick = () => {
+		setClicked(true)
+		copy(text, {
+			format: 'text/plain',
+		})
+		setTimeout(() => {
+			setClicked(false)
+		}, 2000)
+	}
 	return (
 		<Tooltip content='Copy to clipboard'>
 			<IconButton
 				className='cui-w-8 cui-h-8 !cui-p-1'
-				onClick={() =>
-					copy(text, {
-						format: 'text/plain',
-					})
+				onClick={() => handleClick()}
+				icon={
+					clicked ? <CheckIcon className='w-6 h-6' /> : <CopyIcon />
 				}
-				icon={<CopyIcon />}
 				type='button'
 			/>
 		</Tooltip>
